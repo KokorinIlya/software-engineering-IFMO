b2d93c066b476191504467ae19bfc6c5c385457f
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-09-29 17:10:43 +0300

    LRU Cache added

---
+++ lru-cache/src/main/kotlin/com/github/kokorin/lru/Cache.kt
@@ -0,0 +1,65 @@
+package com.github.kokorin.lru
+
+/**
+ * Cache, that stores non-null key-value pairs
+ * @author Ilya Kokorin, kokorin.ilya.1998@yandex.ru
+ */
+interface Cache<K, V> : Iterable<Pair<K, V>> {
+    /**
+     * Returns value, associated with key, if key is present in cache.
+     * Otherwise, returns null
+     * @param key key to get associated value
+     * @return Value, associated with the specified key, if key is present in cache, null otherwise.
+     */
+    fun get(key: K): V?
+
+    /**
+     * Associates specified value with specified key. If some other value was associated with
+     * the specified key before, it is replaced by new value. If no value was associated with
+     * the specified key, adds new key-value pair to the cache.
+     * @param key key to associate value with
+     * @param value value to associate with key
+     * @return old value, that was associated with the specified key before. If no value
+     * was associated with the specified key before insertion, null is returned
+     */
+    fun put(key: K, value: V): V?
+
+    /**
+     * If some value was associated with the specified key, deletes this key-value pair from cache,
+     * returning deleted value. Otherwise, does not modify cache, returning null.
+     * @param key key to delete associated value, if present.
+     * @return deleted value, if key was presented in the cache, null otherwise.
+     */
+    fun delete(key: K): V?
+
+    /**
+     * Checks, if key is presented in cache.
+     * @param key key to check presence of associated value
+     * @return true, if some value is associated with the specified key, false otherwise
+     */
+    fun contains(key: K): Boolean
+
+    /**
+     * Returns number of key-value pairs in cache
+     * @return cache size
+     */
+    fun size(): Int
+
+    /**
+     * Checks, if cache contains zero key-value pairs
+     * @return true, if cache size is zero, false otherwise
+     */
+    fun isEmpty(): Boolean = size() == 0
+
+    /**
+     * Checks, if cache contains at leas one key-value pair
+     * @return true, if cache size is not equal to zero, false otherwise
+     */
+    fun nonEmpty(): Boolean = !isEmpty()
+
+    /**
+     * Gets iterator, that can be used to traverse cache content
+     * @return iterator to traverse cache as a collection of key-value pairs
+     */
+    override fun iterator(): Iterator<Pair<K, V>>
+}
---
+++ lru-cache/src/test/kotlin/LRUCacheTest.kt
@@ -0,0 +1,296 @@
+import com.github.kokorin.lru.LRUCache
+import org.junit.Test
+import org.junit.Assert.*
+
+class LRUCacheTest {
+    @Test
+    fun emptyCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        assertEquals(cache.size(), 0)
+        assertEquals(cache.cacheSize, 5)
+        assertEquals(cache.isFull(), false)
+        assertEquals(cache.nonFull(), true)
+        assertEquals(cache.isEmpty(), true)
+        assertEquals(cache.nonEmpty(), false)
+    }
+
+    @Test
+    fun trivialAddCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        assertTrue(cache.iterator().asSequence().toList() == listOf<Pair<Int, String>>())
+        cache.put(1, "1")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(1, "1")))
+        cache.put(3, "3")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "3"), Pair(1, "1")))
+        cache.put(5, "5")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+    }
+
+    @Test
+    fun getCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.get(2), null)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.get(3), "3")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "3"), Pair(5, "5"), Pair(1, "1")))
+        assertEquals(cache.get(4), null)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "3"), Pair(5, "5"), Pair(1, "1")))
+        assertEquals(cache.get(1), "1")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(1, "1"), Pair(3, "3"), Pair(5, "5")))
+    }
+
+    @Test
+    fun containsCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.contains(2), false)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.contains(3), true)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.contains(4), false)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.contains(1), true)
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+    }
+
+    @Test
+    fun deleteCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.delete(5), "5")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "3"), Pair(1, "1")))
+    }
+
+    @Test
+    fun updateCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        assertEquals(cache.put(3, "33"), "3")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "33"), Pair(5, "5"), Pair(1, "1")))
+    }
+
+    @Test
+    fun addInvalidateCacheTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        assertTrue(cache.iterator().asSequence().toList() == listOf<Pair<Int, String>>())
+        cache.put(1, "1")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(1, "1")))
+        cache.put(3, "3")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(3, "3"), Pair(1, "1")))
+        cache.put(5, "5")
+        assertTrue(cache.iterator().asSequence().toList() == listOf(Pair(5, "5"), Pair(3, "3"), Pair(1, "1")))
+        cache.put(2, "2")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(2, "2"),
+                Pair(5, "5"),
+                Pair(3, "3"),
+                Pair(1, "1")
+            )
+        )
+        cache.put(4, "4")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(5, "5"),
+                Pair(3, "3"),
+                Pair(1, "1")
+            )
+        )
+        cache.put(6, "6")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(6, "6"),
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(5, "5"),
+                Pair(3, "3")
+            )
+        )
+        cache.put(8, "8")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(8, "8"),
+                Pair(6, "6"),
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(5, "5")
+            )
+        )
+        cache.put(9, "9")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(9, "9"),
+                Pair(8, "8"),
+                Pair(6, "6"),
+                Pair(4, "4"),
+                Pair(2, "2")
+            )
+        )
+        assertEquals(cache.get(2), "2")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(2, "2"),
+                Pair(9, "9"),
+                Pair(8, "8"),
+                Pair(6, "6"),
+                Pair(4, "4")
+            )
+        )
+        assertEquals(cache.put(10, "10"), null)
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(10, "10"),
+                Pair(2, "2"),
+                Pair(9, "9"),
+                Pair(8, "8"),
+                Pair(6, "6")
+            )
+        )
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun iteratorTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+        val iter = cache.iterator()
+        while (true) {
+            iter.next()
+        }
+    }
+
+    @Test
+    fun addDeleteTest() {
+        val cache: LRUCache<Int, String> = LRUCache(5)
+        cache.put(1, "1")
+        cache.put(3, "3")
+        cache.put(5, "5")
+        cache.put(7, "7")
+        cache.put(9, "9")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(9, "9"),
+                Pair(7, "7"),
+                Pair(5, "5"),
+                Pair(3, "3"),
+                Pair(1, "1")
+            )
+        )
+        assertEquals(cache.delete(1), "1")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(9, "9"),
+                Pair(7, "7"),
+                Pair(5, "5"),
+                Pair(3, "3")
+            )
+        )
+        cache.put(2, "2")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(2, "2"),
+                Pair(9, "9"),
+                Pair(7, "7"),
+                Pair(5, "5"),
+                Pair(3, "3")
+            )
+        )
+        cache.put(4, "4")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(9, "9"),
+                Pair(7, "7"),
+                Pair(5, "5")
+            )
+        )
+        assertEquals(cache.delete(10), null)
+        assertEquals(cache.delete(9), "9")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(7, "7"),
+                Pair(5, "5")
+            )
+        )
+        assertEquals(cache.delete(9), null)
+        assertEquals(cache.delete(5), "5")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(7, "7")
+            )
+        )
+        assertEquals(cache.put(11, "11"), null)
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(11, "11"),
+                Pair(4, "4"),
+                Pair(2, "2"),
+                Pair(7, "7")
+            )
+        )
+        assertEquals(cache.put(7, "77"), "7")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(7, "77"),
+                Pair(11, "11"),
+                Pair(4, "4"),
+                Pair(2, "2")
+            )
+        )
+        assertEquals(cache.put(4, "44"), "4")
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(4, "44"),
+                Pair(7, "77"),
+                Pair(11, "11"),
+                Pair(2, "2")
+            )
+        )
+        assertEquals(cache.put(25, "25"), null)
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(25, "25"),
+                Pair(4, "44"),
+                Pair(7, "77"),
+                Pair(11, "11"),
+                Pair(2, "2")
+            )
+        )
+        assertEquals(cache.put(17, "17"), null)
+        assertTrue(
+            cache.iterator().asSequence().toList() == listOf(
+                Pair(17, "17"),
+                Pair(25, "25"),
+                Pair(4, "44"),
+                Pair(7, "77"),
+                Pair(11, "11")
+            )
+        )
+    }
+
+}
