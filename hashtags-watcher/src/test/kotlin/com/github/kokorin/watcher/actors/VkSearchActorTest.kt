381bd2825e4b90e510eb7f32d17c78f786f28833
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-10-05 19:40:15 +0300

    mockk added

--- hashtags-watcher/src/test/kotlin/com/github/kokorin/watcher/actors/VkSearchActorTest.kt
+++ hashtags-watcher/src/test/kotlin/com/github/kokorin/watcher/actors/VkSearchActorTest.kt
@@ -8,10 +8,12 @@ import com.github.kokorin.watcher.time.TimeConverter
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.Assert.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class VkSearchActorTest {
     @Test
@@ -26,6 +28,29 @@ class VkSearchActorTest {
         var sended = 0
         coEvery { vkClient.searchHashTag(hashTag, startTime, endTime) } returns VkResponse(Response(25))
         coEvery { parentMailbox.send(VkTimedResponse(5, 25)) } answers {
+            sended += 1
+        }
+
+        val searchActor = VkSearchActor(parentMailbox, vkClient, hashTag, timeConverter)
+        searchActor.makeSingleRequest(hour)
+        assertEquals(sended, 1)
+    }
+
+    @Test
+    fun testTimeout() = runBlocking {
+        val timeConverter = TimeConverter(Date())
+        val hour = 5
+        val (startTime, endTime) = timeConverter.getStartAndEndTime(5)
+        val vkClient = mockk<AsyncVkClient>()
+        val hashTag = "вконтакте"
+        val parentMailbox = mockk<Channel<VkTimedResponse>>()
+
+        var sended = 0
+        coEvery { vkClient.searchHashTag(hashTag, startTime, endTime) } coAnswers  {
+            delay(TimeUnit.MINUTES.toMillis(2))
+            VkResponse(Response(25))
+        }
+        coEvery { parentMailbox.send(VkTimedResponse(5, -1)) } answers {
             sended += 1
         }
 
