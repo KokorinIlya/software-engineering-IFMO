056ce183c887a7aae0fb8af8f06ba2b94801b24d
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-11-11 20:59:27 +0300

    Hw6 finished

--- calculator/src/main/kotlin/com/github/kokorin/calculator/Main.kt
+++ calculator/src/main/kotlin/com/github/kokorin/calculator/Main.kt
@@ -7,8 +7,9 @@ import com.github.kokorin.calculator.visitor.ConvertToRPNVisitor
 import com.github.kokorin.calculator.visitor.PrintVisitor
 
 fun main() {
-    val input = readLine() ?: throw IllegalArgumentException("Input string shouldn't be null")
     try {
+        val input = readLine() ?: throw IllegalArgumentException("Input string shouldn't be null")
+
         val tokenizer = Tokenizer()
         val tokens = tokenizer.tokenize(input)
 
