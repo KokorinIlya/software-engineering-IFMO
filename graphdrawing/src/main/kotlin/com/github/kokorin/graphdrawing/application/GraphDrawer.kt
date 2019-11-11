e3350c0442ff99c557af95d0270a63cbb69e0295
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-11-11 21:08:47 +0300

    PM added

--- graphdrawing/src/main/kotlin/com/github/kokorin/graphdrawing/application/GraphDrawer.kt
+++ graphdrawing/src/main/kotlin/com/github/kokorin/graphdrawing/application/GraphDrawer.kt
@@ -6,11 +6,11 @@ import com.github.kokorin.graphdrawing.graph.MatrixGraph
 
 class GraphDrawer(private val drawingApi: DrawingApi) {
     fun readAndDrawGraph(graphType: GraphType) {
-        val graph = if (graphType == GraphType.LIST) {
-            ListGraph.readListGraph(drawingApi)
-        } else {
-            MatrixGraph.readMatrixGraph(drawingApi)
+        val graph = when (graphType) {
+            GraphType.LIST -> ListGraph.readListGraph(drawingApi)
+            GraphType.MATRIX -> MatrixGraph.readMatrixGraph(drawingApi)
         }
+
         graph.drawGraph()
     }
 }
